98c9894b73fb0dbe2a2bc5db02652410af10429e
Danix-SR <sergiorincon50@gmail.com>
2020-12-07 21:44:19 -0500

    Clases

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 
+class Usuario(object):
+    def __init__(self,Id,Nombre,Apellido):
+        self.Id = Id
+        self.Nombre = Nombre
+        self.Apellido = Apellido
+Lista_User = []
+for cnt in range(0, 5):
+    Lista_User.append(Usuario(str(cnt),"Nombre"+str(cnt),"Apellido"+str(cnt)))
+
 def Index (request):
-    return render(request,"Index.html")
+    return render(request,"Index.html",{"Lista_User":Lista_User})
