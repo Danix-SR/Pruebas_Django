8c2086adc1eee8cc7ad45c68cc3f7294693ed962
Danix-SR <sergiorincon50@gmail.com>
2020-12-09 13:14:46 -0500

    SQLite_Prueba 1

--- app/Usuarios/models.py
+++ app/Usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario (models.Model):
+    Nombre = models.CharField(max_length=50)
+    Apellido = models.CharField(max_length=50)
+    Correo = models.EmailField()
+    Contraseña = models.CharField(max_length=50)
+    Rol = models.CharField(max_length=50)
+    Estado = models.BooleanField()
+
